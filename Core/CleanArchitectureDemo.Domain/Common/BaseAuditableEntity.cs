45dda246d6f2d2eea81aff12fc77f2903fa0883c
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-07 01:17:53 +0300

    add class BaseAuditableEntity

---
+++ Core/CleanArchitectureDemo.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitectureDemo.Domain.Common.Interfaces;
+
+namespace CleanArchitectureDemo.Domain.Common
+{
+    /// <summary>
+    /// This class is the child class of BaseEntity and it implements the IAuditableEntity interface defined above.
+    /// </summary>
+    public abstract class BaseAuditableEntity : BaseEntity, IAuditableEntity
+    {
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
