2f67ab7c4d33ed1ec928c39fbdc3fc8352b22b80
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-07 01:42:23 +0300

    add IPlayerRepository summary

--- Core/CleanArchitectureDemo.Application/IRepositories/IPlayerRepository.cs
+++ Core/CleanArchitectureDemo.Application/IRepositories/IPlayerRepository.cs
@@ -2,6 +2,12 @@
 
 namespace CleanArchitectureDemo.Application.IRepositories
 {
+    /// <summary>
+    /// Most of the time, repositories will only use the generic methods defined 
+    /// in the IGenericRepository class but if they have some additional functionality and 
+    /// require a custom method implementation then these methods can be defined in specific repository interfaces. 
+    /// In the following example, IPlayerRepository is defining an additional method to get players by the club.
+    /// </summary>
     public interface IPlayerRepository
     {
         Task<List<Player>> GetPlayersByClubAsync(int clubId);
