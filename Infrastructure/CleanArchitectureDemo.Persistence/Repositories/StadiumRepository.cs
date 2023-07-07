a0ad8d939389a145bded0e4f44b6a584e85bae33
Aly Mamdouh <aly.momdouh@gmail.com>
2023-07-07 16:04:59 +0300

    add implementation for UnitOfWork ,GenericRepository, and all entities Repositories

---
+++ Infrastructure/CleanArchitectureDemo.Persistence/Repositories/StadiumRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitectureDemo.Application.IRepositories;
+using CleanArchitectureDemo.Domain.Entities;
+
+namespace CleanArchitectureDemo.Persistence.Repositories
+{
+    public class StadiumRepository : IStadiumRepository
+    {
+        private readonly IGenericRepository<Stadium> _repository;
+
+        public StadiumRepository(IGenericRepository<Stadium> repository)
+        {
+            _repository = repository;
+        }
+    }
+}
